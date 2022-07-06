057be5e5a41c2a1adcce8bfc4c434d675d79b193
Nicolas AUVRAY <nicolas-auvray_student2022@wilder.school>
2022-07-06 11:16:36 +0200

    modification de l'appel des composants dans app.jsx et correction des erreurs prettier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import Header from "./frontuser/components/Header";
 import Body from "./frontuser/components/Body";
 import Footer from "./frontuser/components/Footer";
 import "./assets/styles/App.css";
-import Admin from "./frontadmin/components/Admin";
 
 export default function App() {
   return (
@@ -10,7 +9,6 @@ export default function App() {
       <Header />
       <Body />
       <Footer />
-      <Admin />
     </div>
   );
 }
