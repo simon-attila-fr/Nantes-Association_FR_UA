e90af84061e8b56713b5b63fb727ec8d73b54047
Nicolas AUVRAY <nicolas-auvray_student2022@wilder.school>
2022-07-06 10:25:40 +0200

    Création du boutton de scroll pour revenir en haut de page

--- frontend/src/frontuser/components/ScrollButton.jsx
+++ frontend/src/frontuser/components/ScrollButton.jsx
@@ -1,30 +1,30 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 // import { FaAngleUp } from "react-icons/fa";
 import "../../assets/styles/ScrollButton.css";
 
 export default function ScrollButton() {
-  const [showTopBtn, setShowTopBtn] = useState(false);
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 400) {
-        setShowTopBtn(true);
-      } else {
-        setShowTopBtn(false);
-      }
-    });
-  }, []);
+  // const [showTopBtn, setShowTopBtn] = useState(false);
+  // useEffect(() => {
+  //   window.addEventListener("scroll", () => {
+  //     if (window.scrollY > 400) {
+  //       setShowTopBtn(true);
+  //     } else {
+  //       setShowTopBtn(false);
+  //     }
+  //   });
+  // }, []);
   // const goToTop = () => {
   //   window.scrollTo({
   //     top: 0,
   //     behavior: "smooth",
   //   });
   // };
-   return (
+  // return (
   //   <div className="top-to-btm">
   //     {" "}
   //     {showTopBtn && (
   //       <FaAngleUp className="icon-position icon-style" onClick={goToTop} />
   //     )}{" "}
   //   </div>
-  );
+  // );
 }
