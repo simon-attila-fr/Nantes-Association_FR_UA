72c9ccef22145e9c68d014c69108551566b57998
Alice Braye <braye.alice@gmail.com>
2022-07-07 10:48:08 +0200

    page admin content home

--- frontend/src/frontuser/components/Body.jsx
+++ frontend/src/frontuser/components/Body.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import ContentHome from "../../frontadmin/pages/ContentHome";
 import Home from "../pages/Home";
 import News from "../pages/News";
 import Involve from "../pages/Involve";
@@ -11,6 +12,7 @@ export default function Body() {
           <Route path="/" element={<Home />} />
           <Route path="/news" element={<News />} />
           <Route path="/involve" element={<Involve />} />
+          <Route path="/admin/content/home" element={<ContentHome />} />
         </Routes>
       </div>
     </div>
