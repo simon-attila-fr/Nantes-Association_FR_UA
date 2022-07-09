7cc61bee33a58d6c4a57fec794a0b8d180277dee
Nicolas AUVRAY <nicolas-auvray_student2022@wilder.school>
2022-07-09 17:46:51 +0200

    pour changer de branche

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./frontuser/components/Header";
 import Footer from "./frontuser/components/Footer";
 import Home from "./frontuser/pages/Home";
-// import News from "./frontuser/pages/News";
 import Involve from "./frontuser/pages/Involve";
 import HomeAdmin from "./frontadmin/components/MenuSideBar/HomeAdmin";
 import NewsAdmin from "./frontadmin/components/MenuSideBar/NewsAdmin";
@@ -15,17 +14,12 @@ export default function App() {
     <div className="app">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}>
-          {/* <Route path="/news" element={<News />} /> */}
-          <Route path="/involve" element={<Involve />} />
-        </Route>
-      </Routes>
-      <Routes>
-        <Route path="/admin/" element={<ButtonDisconnect />}>
-          <Route path="/admin/accueil" element={<HomeAdmin />} />
-          <Route path="/admin/actualites" element={<NewsAdmin />} />
-          <Route path="/admin/donation" element={<DonsAdmin />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/involve" element={<Involve />} />
+        <Route path="/admin/" element={<ButtonDisconnect />} />
+        <Route path="/admin/accueil" element={<HomeAdmin />} />
+        <Route path="/admin/actualites" element={<NewsAdmin />} />
+        <Route path="/admin/donation" element={<DonsAdmin />} />
       </Routes>
       <Footer />
     </div>
