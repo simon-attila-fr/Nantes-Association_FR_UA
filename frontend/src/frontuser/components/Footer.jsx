c20729c5cf32a4e1bfb6717bdb5f037714c48a08
Nicolas AUVRAY <nicolas-auvray_student2022@wilder.school>
2022-07-10 19:34:29 +0200

    création des menus accordéons pour les mentions juridiques du footer

--- frontend/src/frontuser/components/Footer.jsx
+++ frontend/src/frontuser/components/Footer.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import { useState } from "react";
 import "../../assets/styles/Footer.css";
 import facebook from "../../assets/facebook.svg";
@@ -78,13 +80,50 @@ export default function Footer() {
               </button>
             </form>
           </div>
-          {/* Lien à venir */}
           <div className="legalMentionsContainer">
-            <h5 className="legalMentions">
-              <a className="legalMentionsTitle" href="Link à venir">
-                Mentions Légales & politique de confidentialité{" "}
-              </a>
-            </h5>
+            <div id="div-accordion" className="container-accordions">
+              <section className="juridique">
+                <h5 className="legalMentions" id="ancre1">
+                  Mentions légales & Politique de Confidentialité
+                </h5>
+                <button className="accordion active" type="button">
+                  Mentions légales
+                </button>
+                <div className="panel">
+                  <p>
+                    L’adresse du site est : https://www. Contact : Marianne –
+                    Nantes – assoavenirukrfr@gmail.com Hébergeur : 1&1 IONOS SE
+                    – Elgendorfer Str. 57 – 56410 Montabaur – Allemagne +49 (0)
+                    721 170 555
+                  </p>
+                </div>
+                <button className="accordion active" type="button">
+                  Politique de Confidentialité
+                </button>
+                <div className="panel">
+                  <p>
+                    Le site n’utilise aucun cookie, ne contient aucune
+                    publicité, aucun traceur, aucune statistique. Aucune
+                    information n’est collectée sur les visiteurs du site. Les
+                    seules informations collectées le sont par l’hébergeur et
+                    concernent les fichiers journaux légaux et obligatoires des
+                    accès au site. Biensûr, lorsque vous adhérez à
+                    l'association, nous collectons les informations que vous
+                    nous avez volontairement transmises à cette fin. Ces données
+                    ne seront utilisées que dans le cadre stricte de
+                    l'association pour gérer l'adhésion mais elles ne seront pas
+                    données ou vendues à des entités tierces. En outre, vous
+                    disposez d'un droit d'accès et de rectification sur vos
+                    données en prenant contact à l'adresse mail suivante :
+                    <a
+                      href="mailto:assoavenirukrfr@gmail.com"
+                      target="_blank"
+                      rel="noreferrer"
+                    />
+                  </p>
+                </div>
+              </section>
+            </div>
           </div>
           <div className="copyrightContainer">
             <p className="copyright">
