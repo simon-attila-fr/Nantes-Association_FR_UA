b0e1dbef4dc1f4397a14db985a96ec45759dd5f3
unknown <mehdi.mouilbeau@hotmail.fr>
2022-07-10 20:26:01 +0200

    composant bienvenu + outlet

--- frontend/src/frontadmin/pages/Welcome.jsx
+++ frontend/src/frontadmin/pages/Welcome.jsx
@@ -3,14 +3,19 @@ import NewsAdmin from "../components/MenuSideBar/NewsAdmin";
 import DonsAdmin from "../components/MenuSideBar/DonsAdmin";
 import ButtonDisconnect from "../components/MenuSideBar/ButtonDisconnect";
 import "../../assets/styles/Welcome.css";
+// eslint-disable-next-line import/order
+import { Outlet } from "react-router-dom";
 
 export default function Welcome() {
   return (
-    <div className="sidebar">
-      <HomeAdmin />
-      <NewsAdmin />
-      <DonsAdmin />
-      <ButtonDisconnect />
+    <div>
+      <div className="sidebar">
+        <HomeAdmin />
+        <NewsAdmin />
+        <DonsAdmin />
+        <ButtonDisconnect />
+      </div>
+      <Outlet />
     </div>
   );
 }
