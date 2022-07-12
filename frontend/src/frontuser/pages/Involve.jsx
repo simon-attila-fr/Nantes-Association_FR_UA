8dcf8f9797039a6c5aaf576b4b4a6c4db49f1bbd
Nicolas AUVRAY <nicolas-auvray_student2022@wilder.school>
2022-07-12 16:13:50 +0200

    sauvegarde avant le changement de branche

--- frontend/src/frontuser/pages/Involve.jsx
+++ frontend/src/frontuser/pages/Involve.jsx
@@ -1,83 +1,87 @@
 import "../../assets/styles/Involve.css";
-import JoinForm from "../components/JoinForm";
-import ContactForm from "../components/ContactForm";
+// import ContactForm from "../components/ContactForm";
+import arrow from "../../assets/img/arrow.png";
+import loveUK from "../../assets/img/loveUK.png";
 
 export default function Involve() {
   return (
-    <div className="involve-section">
-      <div className="donate-block">
-        <h2 className="donate-title">Donnez !</h2>
-        <div className="donate-content">
-          <p className="donate-content-text">
-            Faire un don c'est nous permettre d'agir, en menant des actions
-            d'aides humanitaires vers les civils ukrainiens victimes de la
-            guerre, et de soutenir les réfugiés en France. Votre don est
-            précieux, nous avons besoin de votre générosité pour intervenir en
-            toute indépendance, afin de soutenir l'Ukraine.
-          </p>
-        </div>
-        <div className="donate-bonus">
-          <p className="donate-bonus-text">
-            Bon à savoir : Votre don vous donne droit à une réduction d'impôts
-            de 75% de son montant dans la limite de 1000 euros de dons (plafond
-            2020). Au-delà, la réduction d’impôt est de 66 % du montant dans la
-            limite de 20 % de vos revenus nets imposables. Vous pouvez par la
-            suite remplir vos coordonnées personnelles, elles sont
-            indispensables pour que nous puissions traiter votre don et vous
-            envoyer un reçu fiscal.
-          </p>
-        </div>
-        <button className="gift-button" type="button">
-          <a href="module de paiement du CM">FAIRE UN DON</a>
-        </button>
-      </div>
+    <div className="container">
+      <h1>
+        <span>Engagez-vousgit !</span>
+        <span>
+          Avec un don, en devenant membre ou en participant à nos actions...
+        </span>
+      </h1>
 
-      <div className="join-block">
-        <h2 className="join-title">Adhérez !</h2>
-        <div className="join-content">
-          <p className="join-content-text">
-            Adhérer à Avenir Franco-Ukrainien c'est soutenir l'Ukraine, en lui
-            apportant un peu de votre temps et de vos compétences, c'est
-            également contribuer au développement de l'association par les
-            participations aux manifestations extérieures : marches pour la
-            paix, expositions culturelles, missions de bénévolat.
-          </p>
+      <div className="card-container">
+        <div className="card-wrapper">
+          <div className="card">
+            <div className="card-front">
+              <h2>Donnez !</h2>
+              <p>
+                Faire un don c'est nous permettre d'agir, en menant des actions
+                d'aides humanitaires vers les civils ukrainiens victimes de la
+                guerre, et de soutenir les réfugiés en France. Votre don est
+                précieux, nous avons besoin de votre générosité pour intervenir
+                en toute indépendance, afin de soutenir l'Ukraine.
+              </p>
+              <img className="arrow-icon" alt="arrow" src={arrow} />
+            </div>
+            <div className="card-back">
+              <p>
+                Votre don vous donne droit à une réduction d'impôts de 75% de
+                son montant dans la limite de 1000 euros de dons (plafond 2020).
+                Au-delà, la réduction d’impôt est de 66 % du montant dans la
+                limite de 20 % de vos revenus nets imposables. Vous pouvez par
+                la suite remplir vos coordonnées personnelles, elles sont
+                indispensables pour que nous puissions traiter votre don et vous
+                envoyer un reçu fiscal.
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="join-bonus">
-          <p className="join-bonus-text">
-            Bon à savoir : Le montant de votre cotisation sera fonction de votre
-            statut de membre : adhérents (15€), adhérents jeunes (8€), ou encore
-            adhérents bienfaiteurs, associations, collectivités publiques, et
-            autres personnes morales ou membres honoraires (100€). Merci de
-            votre engagement et à bientôt parmi nous !
-          </p>
-        </div>
-        <div className="join-form">
-          <p className="join-form-text">
-            Remplissez le formulaire ci-dessous en complétant bien tous les
-            champs afin de devenir adhérent de l'Association. Nous reviendrons
-            vers vous dès que votre adhésion sera enregistrée pour le réglement
-            de votre cotisation et l'envoi de votre reçu fiscal.
-          </p>
-        </div>
-        <JoinForm />
-        <button className="join-button" type="button">
-          <a href="formulaire d'adhésion.pdf" download>
-            Télécharger le formulaire
-          </a>
-        </button>
-      </div>
 
-      <div className="contact-block">
-        <h2 className="contact-title">Ecrivez-nous !</h2>
-        <div className="contact-content">
-          <p className="contact-content-text">
-            N'hésitez pas à prendre contact avec nous pour en savoir plus sur
-            l'Association, son fonctionnement, devenir membre et participer à
-            nos actions.
-          </p>
+        <div className="card-wrapper">
+          <div className="card">
+            <div className="card-front">
+              <h2>Adhérez !</h2>
+              <p>
+                Adhérer à Avenir Franco-Ukrainien c'est soutenir l'Ukraine, en
+                lui apportant un peu de votre temps et de vos compétences, c'est
+                également contribuer au développement de l'association par les
+                participations aux manifestations extérieures : marches pour la
+                paix, expositions culturelles, missions de bénévolat.
+              </p>
+              <img className="arrow-icon" alt="arrow" src={arrow} />
+            </div>
+            <div className="card-back">
+              <p>
+                Le montant de votre cotisation sera fonction de votre statut de
+                membre : adhérents (15€), adhérents jeunes (8€), ou encore
+                adhérents bienfaiteurs, associations, collectivités publiques,
+                et autres personnes morales ou membres honoraires (100€). Merci
+                de votre engagement et à bientôt parmi nous !
+              </p>
+            </div>
+          </div>
         </div>
-        <ContactForm />
+
+        <div className="card-wrapper">
+          <div className="card">
+            <div className="card-front">
+              <h2>Contactez-nous !</h2>
+              <p>
+                N'hésitez pas à prendre contact avec nous pour en savoir plus
+                sur l'Association, son fonctionnement, devenir membre et
+                participer à nos actions.
+              </p>
+              <img className="arrow-icon" alt="arrow" src={arrow} />
+            </div>
+            <div className="card-back">
+              <img className="loveUK" alt="love" src={loveUK} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
