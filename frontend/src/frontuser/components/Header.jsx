7155d0e6f4b0c037474601384527d4e02c9e09b1
Nicolas AUVRAY <nicolas-auvray_student2022@wilder.school>
2022-07-14 03:13:44 +0200

    nouveau header en version 1

--- frontend/src/frontuser/components/Header.jsx
+++ frontend/src/frontuser/components/Header.jsx
@@ -1,26 +1,51 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable no-unused-vars */
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import GlobalContext from "../contexts/GlobalContext";
-import Don from "./Don";
-import MenuBurger from "./MenuBurger";
+import logo from "../../assets/img/logo.png";
+import FR from "../../assets/img/FR.png";
+import UK from "../../assets/img/UK.png";
 import Translate from "./Translate";
+import compass from "../../assets/img/compass.png";
 import "../../assets/styles/Header.css";
 
 export default function Header() {
   const { setLang } = useContext(GlobalContext);
-  const [showLinks, setShowLinks] = useState(false);
-
-  const handleShowLinks = () => {
-    setShowLinks(!showLinks);
-  };
 
   return (
-    <div>
-      <nav className={`navbar ${showLinks ? "show_nav" : null}  `}>
-        <Don />
-        <Translate />
-        <MenuBurger handleShowLinks={handleShowLinks} />
-      </nav>
-    </div>
+    <nav className="nav-main">
+      <div className="nav-logo">
+        <a
+          href="#"
+          className="nav-logo-link"
+          aria-label="visit homepage"
+          aria-current="page"
+        >
+          <img className="nav-logo-pic" src={logo} alt="logo-pic" />
+        </a>
+      </div>
+
+      <div className="nav-translation">
+        <a href="#" className="nav-flag" aria-label="translation">
+          <img className="lang-flag" src={FR} alt="fench-icon" />
+          <img className="lang-flag" src={UK} alt="ukrainian-icon" />
+        </a>
+        <div className="flag-btns">
+          <Translate />
+        </div>
+      </div>
+
+      <div className="nav-menu">
+        <a href="#" className="menu-toggler" aria-label="page menu">
+          <img className="menu-icon" src={compass} alt="menu-icon" />
+        </a>
+        <a href="#" className="menu-btn1">
+          <span className="menu-span-btn1">Accueil</span>
+        </a>
+        <a href="#" className="menu-btn2">
+          <span className="menu-span-btn2">Faites la différence !</span>
+        </a>
+      </div>
+    </nav>
   );
 }
