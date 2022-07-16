c34128009af71a9823abb623e00220b5950571d2
Nicolas AUVRAY <nicolas-auvray_student2022@wilder.school>
2022-07-17 00:52:47 +0200

    changement des butons par des drapeaux

--- frontend/src/frontuser/components/Translate.jsx
+++ frontend/src/frontuser/components/Translate.jsx
@@ -1,17 +1,22 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable no-unused-vars */
 import { useContext } from "react";
 import GlobalContext from "../contexts/GlobalContext";
+import "../../assets/styles/Translate.css";
+import FR from "../../assets/img/FR.png";
+import UK from "../../assets/img/UK.png";
 
 export default function Translate() {
   const { setLang } = useContext(GlobalContext);
   return (
     <div className="lang-choice">
-      <button type="button" onClick={(e) => setLang("FR")}>
-        Français
-      </button>
-      <button type="button" onClick={(e) => setLang("UK")}>
-        українська
-      </button>
+      <div onClick={(e) => setLang("FR")}>
+        <img className="lang-flag" src={FR} alt="fench-icon" />
+      </div>
+      <div onClick={(e) => setLang("UK")}>
+        <img className="lang-flag" src={UK} alt="ukrainian-icon" />
+      </div>
     </div>
   );
 }
