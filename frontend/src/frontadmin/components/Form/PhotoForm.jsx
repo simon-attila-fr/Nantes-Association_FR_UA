479757dc2a9979994d000f6e5ac294d6b780f0ec
Attila Simon <simon.attila.fr@gmail.com>
2022-07-16 18:07:03 +0200

    Add some css + update PhotoFormDetail

--- frontend/src/frontadmin/components/Form/PhotoForm.jsx
+++ frontend/src/frontadmin/components/Form/PhotoForm.jsx
@@ -1,4 +1,6 @@
+import PropTypes from "prop-types";
 import PhotoFormDetail from "./PhotoFormDetail";
+import "../../../assets/styles/PhotosAdmin.css";
 
 export default function PhotoForm({ content }) {
   return (
@@ -7,6 +9,7 @@ export default function PhotoForm({ content }) {
         <h2>Page d'accueil / Mosaïque</h2>
       </section>
       <section className="photoMosaiqueZone">
+        {/* eslint-disable-next-line react/prop-types */}
         {content.map((e) => {
           return <PhotoFormDetail key={e.id} initialContent={e} />;
         })}
@@ -14,3 +17,20 @@ export default function PhotoForm({ content }) {
     </div>
   );
 }
+
+PhotoForm.propTypes = {
+  content: PropTypes.shape({
+    id: PropTypes.number,
+    photo_name: PropTypes.string,
+    photo_url: PropTypes.string,
+  }),
+};
+
+PhotoForm.defaultProps = {
+  content: PropTypes.shape({
+    id: 1,
+    photo_name: "mosaic1",
+    photo_url:
+      "http://localhost:5000/assets/images/ProfilAttila.png1657924240178.png",
+  }),
+};
