db4e3246adbadd3d45478279037fd825c1f07f2e
Attila Simon <simon.attila.fr@gmail.com>
2022-07-17 23:20:50 +0200

    Landing page animation added

--- frontend/src/frontadmin/components/MenuSideBar/ButtonDisconnect.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-export default function ButtonDisconnect() {
-  return (
-    <button type="button">
-      <Link to="/admin/login"> Se déconnecter </Link>
-    </button>
-  );
-}
--- frontend/src/frontadmin/components/MenuSideBar/DonsAdmin.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-export default function DonsAdmin() {
-  return (
-    <div>
-      <Link to="/admin/donation">Donation / Adhérer </Link>
-    </div>
-  );
-}
