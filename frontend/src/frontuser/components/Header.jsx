54b63ce092c0ad77fba97b93e9419dba45c336fa
misscrazy44 <mehdi.mouilbeau@hotmail.fr>
2022-07-28 12:29:56 +0200

    fix header

--- frontend/src/frontuser/components/Header.jsx
+++ frontend/src/frontuser/components/Header.jsx
@@ -25,10 +25,9 @@ export default function Header() {
       showConfirmButton: false,
       timer: 1500,
     });
-
+  };
   const handleShowLinks = () => {
     setShowLinks(!showLinks);
-
   };
 
   return (
